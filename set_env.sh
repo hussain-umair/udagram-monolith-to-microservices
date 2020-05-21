fe4edc5fe66f987537f65285a46b0144af89decc
MrLongBeard <umair.hussain96@gmail.com>
2020-05-22 03:10:48 +0500

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=umair
+export POSTGRES_PASSWORD=umairhussain
+export POSTGRES_HOST=cdr.c3elwqveng6j.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=cdrdb
+export AWS_BUCKET=test-microservices-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
